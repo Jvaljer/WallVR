50a11cafa110673e7cd56a4859c7c9d8a30c5295
Jvaljer <abbel.haol@gmail.com>
2023-05-05 14:43:21 +0200

    both moving at the same time -> must correct something with the offset calculation (overgrows)

--- Code/Wall_PreWork/Assets/Scripts/Shape.cs
+++ Code/Wall_PreWork/Assets/Scripts/Shape.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shape : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
-    }
+    private Vector3 screen_center = new Vector3(-4.5f, 0f, 0f);
+    private Vector3 pane_center = new Vector3(6.5f, 0f, 10f);
 
     //move method from small's coordinates in world
     public void MoveOnScreen(Vector3 small_pos){
         //here we wanna translate these coords to the screen's world position
-        Debug.Log(small_pos);
-        Debug.Log(transform.position);
+        //first we wanna get the distance between pane_center & small_pos 
+        float dist_x = (small_pos.x - pane_center.x)/3;
+        float dist_y = (small_pos.y - pane_center.y)/3;
+
+        Vector3 new_vec = new Vector3(dist_x*5, dist_y*5, small_pos.z);
+        Debug.Log("new vec -> "+new_vec);
+        transform.position = screen_center+new_vec;
     }
 }
