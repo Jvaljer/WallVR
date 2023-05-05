c63890fa694f584576398cc1380e9b0916b62e5b
Jvaljer <abbel.haol@gmail.com>
2023-05-05 08:54:27 +0200

    instantiated all Photon entities

--- Code/Wall_PreWork/Assets/Scripts/PhotonScripts/Spawner.cs
+++ Code/Wall_PreWork/Assets/Scripts/PhotonScripts/Spawner.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 using Photon.Pun;
 
 public class Spawner : MonoBehaviourPunCallbacks {
-    //must make sure that I have to implement this
+    private GameObject up_left;
+    private GameObject up_right;
+    private GameObject down_left;
+    private GameObject down_right;
+    private GameObject user_prefab;
+
+    //scene attributes
+    private GameObject scene;
+    private Scene scene_script;
+    private GameObject screens;
+
+    //Unity start method 
+    public void Start(){
+        Debug.Log("Starting the Spawner class");
+        //we first need to instantiate all the dependencies
+        scene =  GameObject.Find("Scene");
+        scene_script = GameObject.Find("Scene").GetComponent<Scene>();
+        screens = scene.transform.GetChild(1).gameObject;
+    }
+
+    //this callbacks is called whenever someone joins the room
+    public override void OnJoinedRoom(){
+        Debug.Log("Spawner callbacks to 'OnJoinedRoom'");
+        base.OnJoinedRoom();
+        //so when an user joins the room we then wanna instantiate the 4 screens entities
+        user_prefab = PhotonNetwork.Instantiate("Prefabs/User", new Vector3(0,0,0), transform.rotation);
+        up_left = PhotonNetwork.Instantiate("Prefabs/PhotonScreen", screens.transform.GetChild(0).position, screens.transform.GetChild(0).rotation);
+        up_right = PhotonNetwork.Instantiate("Prefabs/PhotonScreen", screens.transform.GetChild(1).position, screens.transform.GetChild(1).rotation);
+        down_left = PhotonNetwork.Instantiate("Prefabs/PhotonScreen", screens.transform.GetChild(2).position, screens.transform.GetChild(2).rotation);
+        down_right = PhotonNetwork.Instantiate("Prefabs/PhotonScreen", screens.transform.GetChild(3).position, screens.transform.GetChild(3).rotation);
+    }
+
+    //this other callback is called whenever someone lefts the room
+    public override void OnLeftRoom(){
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(user_prefab);
+        PhotonNetwork.Destroy(up_left);
+        PhotonNetwork.Destroy(up_right);
+        PhotonNetwork.Destroy(down_left);
+        PhotonNetwork.Destroy(down_right);
+    }
 }
