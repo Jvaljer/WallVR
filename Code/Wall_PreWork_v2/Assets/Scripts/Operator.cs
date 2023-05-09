9fb376328020de291907cc7d2a5ef1beb725bf44
Jvaljer <abbel.haol@gmail.com>
2023-05-09 10:55:20 +0200

    successfully implemented all on operator view -> now going on shape display on others screens

--- Code/Wall_PreWork_v2/Assets/Scripts/Operator.cs
+++ Code/Wall_PreWork_v2/Assets/Scripts/Operator.cs
@@ -5,6 +5,9 @@ using Photon.Pun;
 using Photon.Realtime;
 
 public class Operator : MonoBehaviourPun {
+    //menu attributes
+    private GameObject menu;
+
     //shapes attributes
     private GameObject circle;
 
@@ -16,17 +19,40 @@ public class Operator : MonoBehaviourPun {
 
     //ready to start predicate
     private bool ready = false;
+    private bool part_ready = false;
+    private bool shape_ready = false;
 
-    // Start is called before the first frame update
-    void Start(){
-        //nothing to initialize yet
+    //owners list
+    private List<GameObject> owners = new List<GameObject>();
+    private List<string> owners_id = new List<string>();
+
+    public void Start(){
+        menu = GameObject.Find("OperatorView/MenuCanva");
     }
-
     // Update is called once per frame
-    void Update(){
+    public void Update(){
+        FetchForShapes();
+        DetermineOwners();
+        /*foreach(GameObject owner in owners){
+            owner.GetComponent<PhotonView>().RPC("ShowCircle", RpcTarget.AllBuffered, circle);
+        }*/
+        SetOwnerInfo();
         //we wanna fetch all the screen parts before we allow the program to start
         if(!ready){
-            FetchForParticipants();
+            if(!part_ready){
+                FetchForParticipants();
+            }
+            if(!shape_ready){
+                FetchForShapes();
+            }
+            ready = part_ready && shape_ready;
+        } else {
+            Debug.Log("determining owners");
+            DetermineOwners();
+            foreach(GameObject owner in owners){
+                owner.GetComponent<PhotonView>().RPC("ShowCircle", RpcTarget.AllBuffered, circle);
+            }
+            SetOwnerInfo();
         }
     }
 
@@ -43,13 +69,109 @@ public class Operator : MonoBehaviourPun {
         if(downright==null){
             downright = GameObject.Find("DownRight");
         }
+        part_ready = upleft!=null && upright!=null && downright!=null && downleft!=null;
     }
     public void FetchForShapes(){
         if(circle==null){
             circle = GameObject.Find("Circle");
         }
+        shape_ready = circle!=null;
     }
 
+    public void DetermineOwners(){
+        //first of all we clean the owners list
+        if(owners!=null){
+            owners.Clear();
+        }
+        if(owners_id!=null){
+            owners_id.Clear();
+        }
+        //then we initiate the needed variables 
+        float x0 = 0f;
+        float y0 = 0f;
+        float r = circle.transform.GetChild(0).gameObject.transform.localScale.x/2;
+        Vector2 coord = new Vector2(circle.transform.position.x, circle.transform.position.y);
+        float x = coord.x;
+        float y = coord.y;
+
+        //now we use a BDT to locate the circle (yet not with the best accuracy) 
+        if(x+r <=x0){
+            if(y-r <=y0){
+                if(y+r <=y0){
+                    //DownLeft
+                    owners.Add(upleft);
+                    owners_id.Add("UpLeft");
+                } else {
+                    //DownLeft + UpLeft
+                    owners.Add(downleft);
+                    owners.Add(upleft);
+                    owners_id.Add("DownLeft");
+                    owners_id.Add("UpLeft");
+                }
+            } else {
+                //UpLeft
+                owners.Add(upleft);
+                owners_id.Add("UpLeft");
+            }
+        } else {
+            if(x-r <= x0){
+                if(y+r <=y0){
+                    //DownRight + DownLeft
+                    owners.Add(downright);
+                    owners.Add(downleft);
+                    owners_id.Add("DownRight");
+                    owners_id.Add("DownLeft");
+                } else {
+                    if(y-r <=y0){
+                        //(worst case) 
+                        //UpLeft + UpRight + DownLeft + DownRight
+                        owners.Add(upleft);
+                        owners.Add(upright);
+                        owners.Add(downleft);
+                        owners.Add(downright);
+                        owners_id.Add("UpLeft");
+                        owners_id.Add("UpRight");
+                        owners_id.Add("DownLeft");
+                        owners_id.Add("DownRight");
+                    } else {
+                        //UpLeft + UpRight
+                        owners.Add(upleft);
+                        owners.Add(upright);
+                        owners_id.Add("UpLeft");
+                        owners_id.Add("UpRight");
+                    }
+                }
+            } else {
+                if(y+r <=y0){
+                    //DownRight
+                    owners.Add(downright);
+                    owners_id.Add("DownRight");
+                } else {
+                    if(y-r <=y0){
+                        //DownRight + UpRight
+                        owners.Add(downright);
+                        owners.Add(upright);
+                        owners_id.Add("DownRight");
+                        owners_id.Add("UpRight");
+                    } else {
+                        //UpRight
+                        owners.Add(upright);
+                        owners_id.Add("UpRight");
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetOwnerInfo(){
+        string str = "";
+        foreach(string id in owners_id){
+            str += id + "\n";
+        }
+        menu.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = str;
+    }
+
+    //RPC methods
     [PunRPC]
     public void SetName(string str){
         gameObject.name = str;
