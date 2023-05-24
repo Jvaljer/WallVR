606afb11e1d4b8d5c924aa198ccecfcb60e77a13
Jvaljer <abbel.haol@gmail.com>
2023-05-24 16:14:04 +0200

    first step of adding the VR -> neither complete nor tested but still coded

--- Code/WallVR/Assets/NetworkHandler.cs
+++ Code/WallVR/Assets/NetworkHandler.cs
@@ -1,18 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class NetworkHandler : MonoBehaviourPunCallbacks {
+    //referenced Setup
+    public Setup setup;
+
+    //photon room
+    private RoomOptions room_opt;
+    private byte max_in_room = 11; //max with Pun FREE (but we need only 11 without VR)
+    public int current_in_room { get; private set; }
+    public bool ope_joined { get; private set; } = false;
+
+    //all used prefabs
+    public GameObject ope_prefab;
+    public GameObject part_2d_prefab;
+    public GameObject part_vr_prefab;
+    public GameObject shape1_prefab;
+    public GameObject shape2_prefab;
+
+    //LocalParticipant & Cameras
+    private GameObject cur_part;
+
+    public void Awake(){
+        //syncing the scenes
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void OnConnectedToMaster(){
+        base.OnConnectedToMaster();
+        //creating the room
+        room_opt = new RoomOptions{MaxPlayers=max_in_room, IsVisible=true, IsOpen=true};
+        PhotonNetwork.JoinOrCreateRoom("Room", room_opt, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom(){
+        base.OnCreatedRoom();
+        //user creates -> MC & init cpt
+        current_in_room = 0;
+        PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+    }
+
+    public override void OnJoinedRoom(){
+        //triggered for the brand newcomer only
+        base.OnJoinedRoom();
+        setup = GameObject.Find("ScriptManager").GetComponent<Setup>();
+        if(setup.is_master){
+            //if master then instantiate operator
+            ope_prefab = PhotonNetwork.Instantiate("Operator", transform.position, transform.rotation);
+
+            shape1_prefab = PhotonNetwork.InstantiateRoomObject("Circle", Vector3.zero, Quaternion.identity);
+            Shape sh1_ctrl = shape1_prefab.GetComponent<Shape>();
+            sh1_ctrl.Categorize("circle");
+            sh1_ctrl.SetSize(shape1_prefab.transform.localScale.x);
+            sh1_ctrl.PositionOn(Vector3.zero);
+        } else {
+            //else instantiate participant
+            if(setup.is_vr){
+                part_vr_prefab = PhotonNetwork.Instantiate("Participant 2D", transform.position, transform.rotation);
+                setup.own_cam= part_vr_prefab.transform.GetChild(0).GetChild(0).GetChild(0).gameObject.GetComponent<Camera>();
+                GameObject.Find("2D Camera").SetActive(false);
+                part_2d_prefab.GetComponent<Participant>().NetworkStart(setup);
+                cur_part = part_vr_prefab;
+            } else {
+                part_2d_prefab = PhotonNetwork.Instantiate("Participant VR", transform.position, transform.rotation);
+                setup.own_cam = GameObject.Find("2D Camera").GetComponent<Camera>();
+                part_2d_prefab.GetComponent<Participant>().NetworkStart(setup);
+                cur_part = part_2d_prefab;
+            }
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer){
+        base.OnPlayerLeftRoom(otherPlayer);
+        cur_part.GetComponent<PhotonView>().RPC("SomeoneLeft", RpcTarget.AllBuffered, otherPlayer.ActorNumber);
+    }
+
+    public void OperatorInitialized(){
+        if(!PhotonNetwork.IsMasterClient){
+            cur_part.GetComponent<PhotonView>().RPC("OperatorStartedRPC", RpcTarget.AllBuffered);
+        }
+    }
+
+    public void Connect(){
+        PhotonNetwork.NickName = System.DateTime.Now.Ticks.ToString();
+        PhotonNetwork.ConnectUsingSettings();
     }
 }
--- Code/WallVR/Assets/Setup.cs
+++ Code/WallVR/Assets/Setup.cs
@@ -1,18 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Setup : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Setup : MonoBehaviourPun {
+    //player role & condition into the program
+    public bool is_master { get; private set; }
+    public int part_cnt { get; private set; }
+    public bool is_vr { get; private set; }
+
+    //wall attributes
+    public float wall_width { get; private set; } 
+    public float wall_height { get; private set; }
+    private string wall_str;
+    public Wall wall { get; private set; }
+
+    //screen attributes (operator screen)
+    public int screen_width { get; set; }
+    public int screen_height { get; set; }
+    public Camera own_cam { get; set; }
+
+    //Screen attributes (participant screen ?)
+    public bool full_screen { get; private set; }
+    public float zoom_ratio { get; private set; }
+
+    //positionning attributes (client screens)
+    public float wall_pos_x { get; private set; }
+    public float wall_pos_y { get; private set; }
+    public float x_pos { get; private set; }
+    public float y_pos { get; private set; }
+
+    public void Awake(){
+        //default values with (1 ope) + (10 2DP) + (1 VRP)
+        is_master = false;
+        full_screen = false;
+        is_vr = false;
+        part_cnt = 11; //not counting ope in these
+
+        string[] args = System.Environment.GetCommandLineArgs();
+        for(int i=0; i<args.Length; i++){
+            switch (args[i]){
+                //wall & screen settings
+                case "-wall":
+                    wall_str = args[i+1];
+                    break;
+                case "-sw": //override by 'screen-width'
+                    screen_width = int.Parse(args[i+1]);
+                    break;
+                case "-sh": //override by 'screen-height'
+                    screen_height = int.Parse(args[i+1]);
+                    break;
+                case "-fs": //override by 'fullscreen'
+                    int fs = int.Parse(args[i+1]);
+                    full_screen = (fs!=0);
+                    break;
+                
+                //user role & condition
+                case "-r":
+                    switch (args[i+1]){
+                        case "m":
+                            is_master = true;
+                            is_vr = false;
+                            break;
+                        case "p2d":
+                            is_master = false;
+                            is_vr = false;
+                            break;
+                        case "pvr":
+                            is_master = false;
+                            is_vr = true;
+                            break;
+                    }
+                    break;
+                case "-x":
+                    x_pos = float.Parse(args[i+1]);
+                    break;
+                case "-y":
+                    y_pos = float.Parse(args[i+1]);
+                    break;
+                
+                //program predicates
+                case "-pa":
+                    part_cnt = int.Parse(args[i+1]);
+                    break;
+                case "-mo":
+                    int v = int.Parse(args[i+1]);
+                    if(v==1){
+                        part_cnt = v;
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+        //setting used wall
+        switch (wall_str){
+            case "WILDER":
+                wall = new Wilder();
+                break;
+            case "DESKTOP":
+                wall = new Desktop(1,2); //Desktop(2,2)
+                break;
+            default:
+                wall = new Wilder();
+                break;
+        }
+        wall_height = wall.Height();
+        wall_width = wall.Width();
+
+        zoom_ratio = 1f; //must accurate
+
+        ConnectToServer();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ConnectToServer(){
+        GameObject.Find("ScriptManager").GetComponent<NetworkHandler>().Connect();
     }
 }
