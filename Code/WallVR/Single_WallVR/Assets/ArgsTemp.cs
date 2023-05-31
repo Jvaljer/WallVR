9ce6be636159777bd345449d65aa96c3a2f7dd01
Jvaljer <abbel.haol@gmail.com>
2023-05-31 08:54:18 +0200

    try commit

--- Code/WallVR/Single_WallVR/Assets/ArgsTemp.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ArgsTemp : MonoBehaviour {
-    //this is just a center script that is meant to contain all arguments and transfer them from Sample(Gateway) to either VR or Wall Scene we're gonna load
-    public static string[] arguments { get; set; }
-}
