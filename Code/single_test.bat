03d47609f778068189441809194bdfe71c6c68b6
Jvaljer <abbel.haol@gmail.com>
2023-05-31 11:41:19 +0200

    some cleaning

--- Code/single_test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set EXEC=WallVR/Single_WallVR.exe
-set WALL=DESKTOP
-
-set PID=p
-
-echo executing for the last part (in VR)
-start "" "%EXEC%" -vr 1 -popuwindow -screen-fullscreen 0 -screen-width 1980 -screen-height 780 -wall %WALL% -sw 1980 -sh 780 -r %PID% -logfile log_vr.txt
